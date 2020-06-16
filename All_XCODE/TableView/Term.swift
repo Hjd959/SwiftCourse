bebdfcfea9d85a6e3d275c61883cd14097c221d5
wahbe7 <a-abdulwahab-77@hotmail.com>
2020-06-16 19:51:28 +0300

    2

---
+++ All_XCODE/TableView/Term.swift
@@ -0,0 +1,9 @@
+//
+//  Term.swift
+//  All_XCODE
+//
+//  Created by عبدالوهاب العنزي on 13/06/2020.
+//  Copyright © 2020 Abdulwahab. All rights reserved.
+//
+
+import Foundation
---
+++ All_XCODE/Using Class in Your App/Classes2.swift
@@ -0,0 +1,30 @@
+//
+//  Classes2.swift
+//  All_XCODE
+//
+//  Created by عبدالوهاب العنزي on 13/06/2020.
+//  Copyright © 2020 Abdulwahab. All rights reserved.
+//
+
+import UIKit
+
+class Classes2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
